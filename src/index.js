82fb8a39b8eb4b65cfaaa2c2c0a913e25beb3cbd
Sujeeth Shingade <108385080+sujeethshingade@users.noreply.github.com>
2024-07-07 03:27:11 +0530

    remove rtl

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import AdminLayout from "layouts/Admin/Admin.js";
-import RTLLayout from "layouts/RTL/RTL.js";
 import "assets/scss/black-dashboard-react.scss";
 import "assets/demo/demo.css";
 import "assets/css/nucleo-icons.css";
@@ -18,7 +17,6 @@ root.render(
       <BrowserRouter>
         <Routes>
           <Route path="/admin/*" element={<AdminLayout />} />
-          <Route path="/rtl/*" element={<RTLLayout />} />
           <Route
             path="*"
             element={<Navigate to="/admin/explore-campaigns" replace />}
