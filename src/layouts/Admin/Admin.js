bc05fd53f967fcd049392cc08975d26b4159fb9b
Sujeeth Shingade <108385080+sujeethshingade@users.noreply.github.com>
2024-07-06 17:24:40 +0530

    change logo

--- src/layouts/Admin/Admin.js
+++ src/layouts/Admin/Admin.js
@@ -1,20 +1,3 @@
-/*!
-
-=========================================================
-* Black Dashboard React v1.2.2
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/black-dashboard-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 import { Route, Routes, Navigate, useLocation } from "react-router-dom";
 // javascript plugin used to create scrollbars on windows
@@ -28,7 +11,7 @@ import FixedPlugin from "components/FixedPlugin/FixedPlugin.js";
 
 import routes from "routes.js";
 
-import logo from "assets/img/react-logo.png";
+import logo from "assets/img/logo.jpeg";
 import { BackgroundColorContext } from "contexts/BackgroundColorContext";
 
 var ps;
